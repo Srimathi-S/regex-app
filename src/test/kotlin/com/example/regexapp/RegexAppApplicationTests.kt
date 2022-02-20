e560e8e3ed6568f8d09d6315d54805f3d7ecfa2c
Srimathi <srimathi.s@thoughtworks.com>
2022-02-20 18:05:50 +0530

    Inital setup

---
+++ src/test/kotlin/com/example/regexapp/RegexAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.regexapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegexAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
